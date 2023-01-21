e6199755fbddacd437c992a18abaf06e7acc365e
mohanbendegumbali <mohan0311@hotmail.com>
2023-01-21 20:38:47 +0530

    Added Number To Word

---
+++ src/com/bridgelabz/NumberToWord.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+import java.util.*;
+
+public class NumberToWord {
+	        public static void main(String[] args) {
+	            Scanner s = new Scanner(System.in);
+	            System.out.print("Enter a single digit number: ");
+	            int n = s.nextInt();
+	            String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	            if (n >= 0 && n <= 9) {
+	                System.out.println(words[n]);
+	            } else {
+	                System.out.println("Invalid number");
+	            }
+	        }
+	    }
+
