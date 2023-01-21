8f5ca2847fc5d27d171f9959987cadb77d521911
mohanbendegumbali <mohan0311@hotmail.com>
2023-01-21 20:23:10 +0530

    Added Palindrome

--- src/com/bridgelabz/ArithmeticOperator.java
+++ src/com/bridgelabz/ArithmeticOperator.java
@@ -1,5 +1,30 @@
 package com.bridgelabz;
+import java.util.*;
 
 public class ArithmeticOperator {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        System.out.print("Enter third number: ");
+        int c = scanner.nextInt();
+
+        int result1 = a + b * c;
+        int result2 = c + a / b;
+        int result3 = a % b + c;
+        int result4 = a * b + c;
+        int max = Math.max(Math.max(result1, result2), Math.max(result3, result4));
+        int min = Math.min(Math.min(result1, result2), Math.min(result3, result4));
+        System.out.println("Result1: " + result1);
+        System.out.println("Result2: " + result2);
+        System.out.println("Result3: " + result3);
+        System.out.println("Result4: " + result4);
+        System.out.println("Max: " + max);
+        System.out.println("Min: " + min);
+    }
 }
+
+
