526aa39ddb0e00bec1173ff0202d8873ba1d18ce
mohanbendegumbali <mohan0311@hotmail.com>
2023-01-21 20:36:47 +0530

    Added Reverse Number For

--- src/com/bridgelabz/Quadratic.java
+++ src/com/bridgelabz/Quadratic.java
@@ -1,5 +1,36 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class Quadratic {
 
-}
+	        public static void main(String[] args) {
+	            Scanner scanner = new Scanner(System.in);
+	            System.out.println("enter val of a : ");
+	            double a = scanner.nextInt();
+	            System.out.println("enter val of b : ");
+	            double b = scanner.nextInt();
+	            System.out.println("enter val of c ; ");
+	            double c = scanner.nextInt();
+	            double delta = b * b - 4 * a * c;
+	            if (delta > 0) {
+	                double root1 = (-b + Math.sqrt(delta)) / (2 * a);
+	                double root2 = (-b - Math.sqrt(delta)) / (2 * a);
+	                System.out.println("Root 1: " + root1);
+	                System.out.println("Root 2: " + root2);
+	            } else if (delta == 0) {
+	                double root = -b / (2 * a);
+	                System.out.println("Root: " + root);
+	            } else {
+	                System.out.println("No real roots.");
+	            }
+	        }
+
+
+
+
+
+
+
+	}
+
--- src/com/bridgelabz/ReverseNumberFor.java
+++ src/com/bridgelabz/ReverseNumberFor.java
@@ -1,16 +1,15 @@
 package com.bridgelabz;
 import java.util.*;
 
-public class ReverseIntNumber {
+public class ReverseNumberFor {
 	    public static void main(String[] args) {
 	        Scanner scanner = new Scanner(System.in);
 	        System.out.print("Enter a number: ");
 	        int number = scanner.nextInt();
 	        int reversed = 0;
-	        while (number != 0) {
+	        for (;number != 0;number=number/10) {
 	            int digit = number % 10;
 	            reversed = reversed * 10 + digit;
-	            number = number / 10;
 	        }
 	        System.out.println("Reversed number: " + reversed);
 	    }
